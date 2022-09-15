c2ecef253d8f42a8975a0b143e3716d1e8e80607
Max Levine <max.levine@elephant.healthcare>
2022-09-15 15:54:42 +0100

    Update apply script for helm

--- apply.sh
+++ apply.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-kubectl delete namespace/max-gitops-exercise
-kubectl apply -f namespace.yaml -f configmap.yaml -f replica_sets/ -f services/ -f virtual_services/
+helm upgrade -i max-nginx max-nginx/
+
